d96ddeee916b707e3fdbf65f1500310c07f6b113
kinyuykelly <kinyuykelly@gmail.com>
2023-08-27 22:42:19 +0100

    creating a declaration file

---
+++ Chapter 2/exercise 2.01/user.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var jon;
+jon = {
+    email: "jon@snow.com",
+    roles: ["admin"]
+};
+var alice;
+alice = {
+    email: "alice@snow.com",
+    roles: ["super_admin"],
+    source: "afacebook"
+};
