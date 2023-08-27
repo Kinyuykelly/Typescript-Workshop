fc91efe13ad81247a8544c70952957dd8cde0993
kinyuykelly <kinyuykelly@gmail.com>
2023-08-28 00:06:41 +0100

    Creating types with external libraries

---
+++ Chapter 2/exercise 2.02/lodash_example.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var _ = require("lodash");
+var nums = [1, 2, 3];
+console.log(_.last(nums));
+var text = "hello";
+console.log(_.last(text));
---
+++ Chapter 2/exercise 2.02/lodash_example.ts
@@ -0,0 +1,5 @@
+import _ = require("lodash");
+const nums = [1,2,3];
+console.log(_.last(nums));
+const text = "hello";
+console.log(_.last(text));
