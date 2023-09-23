73d8ab9c97be3822ee7f0ac9eb14a8c22cd562e9
kinyuykelly <kinyuykelly@gmail.com>
2023-09-23 21:47:36 +0100

    Building my first class

---
+++ Chapter 4/exercise 4.01/scoreboard.js
@@ -0,0 +1,11 @@
+"use strict";
+var Team = /** @class */ (function () {
+    function Team() {
+    }
+    Team.prototype.generateLineup = function () {
+        return "Lineup will go here...";
+    };
+    return Team;
+}());
+var astros = new Team();
+console.log(astros.generateLineup());
---
+++ Chapter 4/exercise 4.01/scoreboard.ts
@@ -0,0 +1,9 @@
+class Team{
+    generateLineup() {
+      return "Lineup will go here...";
+    }
+}
+const astros = new Team();
+console.log(astros.generateLineup())
+
+
