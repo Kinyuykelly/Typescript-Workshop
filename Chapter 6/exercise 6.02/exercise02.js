75207605ae5454c860052c0e49541123396810a4
kinyuykelly <kinyuykelly213@gmail.com>
2023-10-11 23:30:49 +0100

    type literals

---
+++ Chapter 6/exercise 6.02/exercise02.js
@@ -0,0 +1,5 @@
+"use strict";
+function onlyNo(no) {
+    return 0;
+}
+console.log(onlyNo("no"));
---
+++ Chapter 6/exercise 6.02/exercise02.ts
@@ -0,0 +1,8 @@
+
+
+type No = "no";
+type Zero = 0;
+function onlyNo(no: No): Zero {
+    return 0;
+}
+console.log(onlyNo("no"))
