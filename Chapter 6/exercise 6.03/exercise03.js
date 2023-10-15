66900bc37a33e38e01553e1210cb43e3f649180a
kinyuykelly <kinyuykelly@gmail.com>
2023-10-15 15:09:22 +0100

    type intersection

---
+++ Chapter 6/exercise 6.03/exercise03.js
@@ -0,0 +1,23 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var superUser = {
+    _id: 1,
+    email: 'rayon.hunte@gmail.com',
+    token: '12345',
+    accesPages: [
+        'profile', 'adminConsole', 'userReset'
+    ],
+    lastLogin: new Date()
+};
+//create backup user
+var backupUser = {
+    _id: 2,
+    email: 'rayon.backup@gmail.com',
+    token: '123456',
+    lastBackUp: new Date(),
+    backUpLocation: './backup'
+};
+//console out super user props
+console.log(superUser);
+//console out backup user props
+console.log(backupUser);
---
+++ Chapter 6/exercise 6.03/exercise03.ts
@@ -0,0 +1,42 @@
+import { type } from "os";
+
+/// create user object type
+type BasicUser = {
+    _id: number;
+    email: string;
+    token: string;
+}
+// create admin object type 
+type Admin = {
+    accesPages: string[];
+    lastLogin: Date;
+}
+//create backup user object type
+type Backup = {
+    lastBackUp: Date;
+    backUpLocation: string;
+}
+const superUser: BasicUser & Admin = {
+    _id: 1,
+    email:  'rayon.hunte@gmail.com',
+    token: '12345',
+    accesPages: [
+        'profile', 'adminConsole', 'userReset' 
+    ],
+    lastLogin: new Date()
+};
+// create backup type
+type BackupUser = BasicUser & Backup;
+//create backup user
+const backupUser: BackupUser = {
+    _id: 2,
+    email: 'rayon.backup@gmail.com',
+    token: '123456',
+    lastBackUp: new Date(),
+    backUpLocation: './backup'
+};
+//console out super user props
+console.log(superUser);
+//console out backup user props
+console.log(backupUser);
+
